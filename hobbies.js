15f5314751235d075f1e144f5a8a75540462c611
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-16 13:37:01 +0100

    feat: add function hobbies with export and import

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree){
+  return {
+    hobbyOne,
+    hobbyTwo,
+    hobbyThree
+  }
+}
+
+module.exports = hobbies;
--- people.js
+++ people.js
@@ -1,3 +1,6 @@
 const fullName = require('./names.js');
+const hobbies = require('./hobbies.js');
 
 console.log(fullName("france", "marc"));
+console.log(hobbies("guitar", "football", "horses"));
+
